9e84fa3166392349b3d510ad1faf7889dab5afda
Jatin <jatinx.lol@gmail.com>
2015-04-07 23:36:53 +0530

    Update class.function.html.php

--- include/functions/class.function.html.php
+++ include/functions/class.function.html.php
@@ -1,10 +1,9 @@
 <?php
    class HTML {
 	     const VERSION = '1.0.1';
-         public $index_html = "/theme/index.html";
+         public $index_html = '/soundcloud/include/theme/index.html';
 		 public function index(){
-			    include($this->index_html);
+			    include($_SERVER['DOCUMENT_ROOT'].$this->index_html);
 		 }
    }
 ?>
-		
